920b21fdf75281e0cb2796797d53f1aabc0d2c30
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-07-22 21:23:47 +0100

    Home Page Render

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mainRoutes = require('./routes/mainRoute');
 
 const PORT = process.env.PORT || 3000;
+
+//View Engines
+app.set('view engine', 'ejs');
 
 //Middleware
 app.use(express.static('public'));
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,5 +3,5 @@
  * GET - Get Home Page
  */
 module.exports.get_home = (req, res) => {
-	res.send('This is the Server From the Controller Folder');
+	res.render('index', { title: "Home Page"})
 }
