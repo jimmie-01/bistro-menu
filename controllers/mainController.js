8a6d741404ab1742ab246be6cb92389d7f05c4f8
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-07-25 09:57:55 +0100

    About Page and Controller

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,5 +3,19 @@
  * GET - Get Home Page
  */
 module.exports.get_home = (req, res) => {
-	res.render('index', { title: "Home Page"})
+	res.render('index', { title: "Home Page"});
+}
+
+/**
+ * GET - Get About Page
+ */
+module.exports.get_about = (req, res) => {
+	res.render('about', { title: "About Page" });
+}
+
+/**
+ * GET - Get Menu List
+ */
+module.exports.get_menu = (req, res) => {
+	res.render('menu', { title: "Menu" });
 }
--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const Controllers = require('../controllers/mainController');
 
 router.get('/', Controllers.get_home);
+router.get('/about', Controllers.get_about);
+router.get('/menu', Controllers.get_menu);
 
 module.exports = router;
