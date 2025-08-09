42de100ba7704e958cdd7c191e82393a692dc207
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-08-09 12:25:17 +0100

    Admin Panel Setup

--- controllers/dashBoardController.js
+++ controllers/dashBoardController.js
@@ -4,6 +4,22 @@ const { MenuItem, DrinkMenu } = require('../models/menuSchema');
  * GET - Get Dashboard Panel
  */
 
-module.exports.get_dashboard  = (req, res) => {
-	res.render('admin/dashBoard', { title: "Admin Panel" });
+module.exports.get_dashboard  = async(req, res) => {
+	try {
+		const Items = await MenuItem.find().sort({ category: 1 });
+		
+		const itemsByCategory = {};
+		items.forEach(item => {
+			if(!itemsByCategory[item.category]){
+				itemsByCategory[item.category] = [];
+			}
+			itemsByCategory[item.category].push(item);
+		});
+		res.render('admin/dashBoard', {
+			 title: "Admin Panel",
+			 itemsByCategory
+			});
+	} catch (error) {
+		console.log(error);
+	}
 }
