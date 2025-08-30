0093024d8ebdb647abe0f757420cf892d5e832fa
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-08-30 12:26:13 +0100

    Role Authorization implemented

--- routes/dashBoard.js
+++ routes/dashBoard.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const Controllers = require('../controllers/dashBoardController');
 const { protect, authorize } = require('../utils/authMiddleware');
 
+router.use(protect);
 
-
-router.get('/dashboard', protect, Controllers.get_dashboard);
+router.get('/dashboard', Controllers.get_dashboard);
 router.get('/dashboard/drinks', Controllers.get_dashboard_drinks);
 router.get('/dashboard/:category', Controllers.get_category_items);
 router.get('/dashboard/drinks/:category', Controllers.get_category_drinkItems);
-router.get('/menu/add-item', Controllers.get_create_menu);
-router.post('/menu/add-item', Controllers.post_create_menu);
-router.get('/edit/:name', Controllers.get_edit_item);
-router.post('/edit/:id', Controllers.post_edit_item);
-router.post('/dashboard/item/:name', Controllers.delete_item);
+router.get('/menu/add-item', authorize('owner', 'general manager'), Controllers.get_create_menu);
+router.post('/menu/add-item', authorize('owner', 'general manager'), Controllers.post_create_menu);
+router.get('/edit/:name', authorize('owner', 'general manager'), Controllers.get_edit_item);
+router.post('/edit/:id', authorize('owner', 'general manager'), Controllers.post_edit_item);
+router.post('/dashboard/item/:name',authorize('owner', 'general manager'), Controllers.delete_item);
 
 module.exports = router;
